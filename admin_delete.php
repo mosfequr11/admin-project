b18eb68f8d71c57c3d77217ab228f5f38ead0874
mosfequr11 <mosfequr11@gmail.com>
2022-02-09 00:14:49 +0600

    add time

--- admin_delete.php
+++ admin_delete.php
@@ -4,18 +4,15 @@
 	$success_message = '';
 	$error_message = '';
 
-
 	//Multiple record delete
 
 	if(isset($_POST['delete_multiple_record_id'])){
-
 		$select_multiple_id = $_POST['delete_record_id'];
 		$user_id = implode(',', $select_multiple_id);
-
 			// echo "<pre>";
 			// var_dump($user_id);
 			// echo "</pre>";
-		// echo $user_id;
+			// echo $user_id;
 				// echo "<pre>";
 				// print_r($select_multiple_id);
 				// echo "</pre>";
@@ -28,22 +25,19 @@
 			$error_message = "Error deleting record: " . $conn->error;
 		}
 	}
-
 	// else
 	// echo "helloworld";
 
-
 	//select row and fetch	
-
-	// if(!empty($_GET['delete_id'])){
-	// 	$user_id = $_GET['delete_id'];
-	// 	print_r($user_id);
-	// 	$sql = "DELETE FROM post_news WHERE id=".$user_id;
-	// 	if ($conn->query($sql) === TRUE) {
-	// 		$success_message = "deleted successfully";
-	// 		header("Location: admin_show.php"); /* Redirect browser */
-	// 	} else {
-	// 		$error_message = "Error deleting record: " . $conn->error;
-	// 	}
-	// }
+	if(!empty($_GET['delete_id'])){
+		$user_id = $_GET['delete_id'];
+		print_r($user_id);
+		$sql = "DELETE FROM post_news WHERE id=".$user_id;
+		if ($conn->query($sql) === TRUE) {
+			$success_message = "deleted successfully";
+			header("Location: admin_show.php"); /* Redirect browser */
+		} else {
+			$error_message = "Error deleting record: " . $conn->error;
+		}
+	}
 	
--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "admin-project";
+	$dbname = "admin-project-git";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
